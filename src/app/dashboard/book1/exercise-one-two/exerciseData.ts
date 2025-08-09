912844635f9576fd8872c49f7e40cacf6e406508
Stiven <strujillodev@gmail.com>
2025-08-08 19:04:31 -0500

    ✨ FEAT: refactor Exercise One and Two component to use external data for titles and items

---
+++ src/app/dashboard/book1/exercise-one-two/exerciseData.ts
@@ -0,0 +1,36 @@
+// exerciseData.ts
+// Variables genéricas para reutilización de ejercicios uno y dos
+
+export const exerciseTitle1 = "Exercise One (Ejercicio 1)";
+export const exerciseTitle2 = "Exercise Two (Ejercicio 2)";
+
+export const exerciseItems1 = [
+  "Enroll her!",
+  "Pass me!",
+  "Don't flunk it!",
+  "Obey us!",
+  "Come to school!",
+  "Behave with us!",
+  "Don't borrow money from him!",
+  "Pass it!",
+  "Don't beat him!",
+  "Enroll me!",
+  "Don't leave me!",
+  "Don't eat it!",
+  "Shout at her!",
+];
+
+export const exerciseItems2 = [
+  "¡Benefícielo (a él)!",
+  "¡No nos abandone (a nosotros)!",
+  "¡No pierdas inglés!",
+  "¡Vaya con (with) ellos!",
+  "¡No venga a la casa (to the house)!",
+  "¡Grítele (a ella)!",
+  "¡No se comporte con ella!",
+  "¡Pídale prestado el libro (the book) a él!",
+  "¡No me matricule (a mí)!",
+  "¡Grítele (a ellos)!",
+  "¡No le obedezca a él!",
+  "¡Compórtese con ella!",
+];
--- src/app/dashboard/book1/exercise-one-two/page.tsx
+++ src/app/dashboard/book1/exercise-one-two/page.tsx
@@ -1,176 +1,45 @@
+
 import { Title1 } from "@/components/title1";
 import { Title2 } from "@/components/title2";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import {
+  exerciseItems1,
+  exerciseItems2,
+  exerciseTitle1,
+  exerciseTitle2,
+} from "./exerciseData";
 
 export default function ExerciseOneAndTwo() {
   return (
     <section className="flex flex-col gap-4">
       <Title1>Exercises</Title1>
-      <Title2>Exercise One (Ejercicio 1)</Title2>
+      <Title2>{exerciseTitle1}</Title2>
       <p className="font-bold">Translate</p>
       <ol className="list-decimal list-outside ml-6 space-y-1">
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Enroll her!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Pass me!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Don't flunk it!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Obey us!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Come to school!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Behave with us!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Don't borrow money from him!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Pass it!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Don't beat him!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Enroll me!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Don't leave me!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Don't eat it!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">Shout at her!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
+        {exerciseItems1.map((text, idx) => (
+          <li key={idx} className="[&::marker]:font-bold">
+            <div className="flex items-center gap-2">
+              <p className="whitespace-nowrap">{text}</p>
+              <Input type="text" className="flex-1 min-w-0" />
+            </div>
+          </li>
+        ))}
       </ol>
 
       <Separator />
 
-      <Title2>Exercise Two (Ejercicio 2)</Title2>
+      <Title2>{exerciseTitle2}</Title2>
       <p className="font-bold">Translate</p>
       <ol className="list-decimal list-outside ml-6 space-y-1">
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡Benefícielo (a él)!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡No nos abandone (a nosotros)!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡No pierdas inglés!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡Vaya con (with) ellos!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">
-              ¡No venga a la casa (to the house)!
-            </p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡Grítele (a ella)!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡No se comporte con ella!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">
-              ¡Pídale prestado el libro (the book) a él!
-            </p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡No me matricule (a mí)!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡Grítele (a ellos)!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡No le obedezca a él!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
-        <li className="[&::marker]:font-bold">
-          <div className="flex items-center gap-2">
-            <p className="whitespace-nowrap">¡Compórtese con ella!</p>
-            <Input type="text" className="flex-1 min-w-0" />
-          </div>
-        </li>
+        {exerciseItems2.map((text, idx) => (
+          <li key={idx} className="[&::marker]:font-bold">
+            <div className="flex items-center gap-2">
+              <p className="whitespace-nowrap">{text}</p>
+              <Input type="text" className="flex-1 min-w-0" />
+            </div>
+          </li>
+        ))}
       </ol>
     </section>
   );
