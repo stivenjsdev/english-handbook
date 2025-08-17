ca20b8e0add7f360dac9c6637cea82a307352df9
Stiven <strujillodev@gmail.com>
2025-08-17 11:57:50 -0500

    ✨ FEAT: add Exercise Six, Seven, and Eight components with exercise data and update Exercise Five page

--- src/app/dashboard/book1/exercise-five/exerciseData.ts
+++ src/app/dashboard/book1/exercise-five/exerciseData.ts
@@ -1,4 +1,7 @@
 export const exerciseTitle1 = "Exercise Five (Ejercicio 5)";
+export const exerciseTitle2 = "Exercise Six (Ejercicio 6)";
+export const exerciseTitle3 = "Exercise Seven (Ejercicio 7)";
+export const exerciseTitle4 = "Exercise Eight (Ejercicio 8)";
 
 export const exerciseItems1 = [
   "Let's buy it instead of them!",
@@ -11,4 +14,44 @@ export const exerciseItems1 = [
   "Let's not walk with them!",
   "Let's work for them!",
   "Let's not freeze it because of him!",
+];
+export const exerciseItems2 = [
+  "¡No lo perdamos (eso) por culpa de ellos!",
+  "¡Comportémonos con ella en clase!",
+  "¡Vamos sin ella a clase!",
+  "¡Comámonos eso en vez de éste!",
+  "¡Trabajemos con este computador en vez de ese!",
+  "¡No perdamos (el examen) por culpa de ellas!",
+  "¡Aprobemos (el examen) con ella!",
+  "¡No nos alejemos del colegio sin ellos!",
+  "¡Reportémoslas (a ellas) en lugar de a ellos!",
+  "¡No la abandonemos (a ella) por culpa de él!",
+  "¡Matricúlela (a ella) con usted, por favor!",
+  "¡No me grite (a mí), por favor!",
+];
+export const exerciseItems3 = [
+  "Let's study with them!",
+  "Let's not work without her!",
+  "Let's borrow the book from them!",
+  "Let's not catch a bird for them!",
+  "Let's not enroll this school because of him!",
+  "Let's leave our dad alone! (solo)",
+  "Let's not shout at her!",
+  "Let's not flunk this subject! (esta materia)",
+  "Let's benefit her instead of us!",
+  "Let's defrost the fish!",
+  "Let's not ride without them!",
+  "Let's study with him!",
+];
+export const exerciseItems4 = [
+  "¡Ponga eso alli (there) para mí, por favor!",
+  "Por favor, ¡Repórtela (a ella) por culpa de él!",
+  "¡No los capture (a ellos) sin ella, por favor!",
+  "¡Cante con ella en vez de ellos, por favor!",
+  "¡No lo pierda (eso) por favor!",
+  "¡Aléjese de ellos por culpa de él, por favor!",
+  "¡Páseme (a mí) en lugar de él por favor!",
+  "¡Obedézcale a Ruth en vez de a Jeanne, por favor!",
+  "¡No se aleje de la Academia de Inglés de Macarthur, por favor!",
+  "¡Trabaje con Randy en lugar de ellos, por favor!",
 ];
--- src/app/dashboard/book1/exercise-five/page.tsx
+++ src/app/dashboard/book1/exercise-five/page.tsx
@@ -2,7 +2,16 @@ import { Title1 } from "@/components/title1";
 import { Title2 } from "@/components/title2";
 import { Input } from "@/components/ui/input";
 import { SquarePen } from "lucide-react";
-import { exerciseItems1, exerciseTitle1 } from "./exerciseData";
+import {
+  exerciseItems1,
+  exerciseItems2,
+  exerciseItems3,
+  exerciseItems4,
+  exerciseTitle1,
+  exerciseTitle2,
+  exerciseTitle3,
+  exerciseTitle4,
+} from "./exerciseData";
 
 export default function ExerciseFive() {
   return (
@@ -15,6 +24,60 @@ export default function ExerciseFive() {
       <p className="font-bold">Translate</p>
       <ol className="list-decimal list-outside ml-6 space-y-1">
         {exerciseItems1.map((text, idx) => (
+          <li key={idx} className="[&::marker]:font-bold">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2">
+              <p className="truncate sm:whitespace-nowrap whitespace-normal sm:mb-0 mb-1 max-w-full">
+                {text}
+              </p>
+              <Input type="text" className="flex-1 min-w-0" />
+            </div>
+          </li>
+        ))}
+      </ol>
+
+      <Title2 className="flex items-center gap-1">
+        <SquarePen className="mt-0.5" />
+        {exerciseTitle2}
+      </Title2>
+      <p className="font-bold">Translate</p>
+      <ol className="list-decimal list-outside ml-6 space-y-1">
+        {exerciseItems2.map((text, idx) => (
+          <li key={idx} className="[&::marker]:font-bold">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2">
+              <p className="truncate sm:whitespace-nowrap whitespace-normal sm:mb-0 mb-1 max-w-full">
+                {text}
+              </p>
+              <Input type="text" className="flex-1 min-w-0" />
+            </div>
+          </li>
+        ))}
+      </ol>
+
+      <Title2 className="flex items-center gap-1">
+        <SquarePen className="mt-0.5" />
+        {exerciseTitle3}
+      </Title2>
+      <p className="font-bold">Translate</p>
+      <ol className="list-decimal list-outside ml-6 space-y-1">
+        {exerciseItems3.map((text, idx) => (
+          <li key={idx} className="[&::marker]:font-bold">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2">
+              <p className="truncate sm:whitespace-nowrap whitespace-normal sm:mb-0 mb-1 max-w-full">
+                {text}
+              </p>
+              <Input type="text" className="flex-1 min-w-0" />
+            </div>
+          </li>
+        ))}
+      </ol>
+
+      <Title2 className="flex items-center gap-1">
+        <SquarePen className="mt-0.5" />
+        {exerciseTitle4}
+      </Title2>
+      <p className="font-bold">Translate</p>
+      <ol className="list-decimal list-outside ml-6 space-y-1">
+        {exerciseItems4.map((text, idx) => (
           <li key={idx} className="[&::marker]:font-bold">
             <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2">
               <p className="truncate sm:whitespace-nowrap whitespace-normal sm:mb-0 mb-1 max-w-full">
